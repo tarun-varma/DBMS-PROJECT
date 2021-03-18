c3046b2a4a89dfea16608eb89b46bcdd32ad0cd8
tarun-varma <datlatarun2001@gmail.com>
2021-03-18 18:54:39 +0530

    deployment

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://socialmediaapp:socialmediaapp1@onlinedatingapp.zpq0l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    GoogleClientID: '457792682124-r2bb9nqpevc4vjk381gpm60g8ebc570a.apps.googleusercontent.com',
+    GoogleClientSecret: '_qiPsIwYhM6YuKjAdaTaqXOW'
+}
